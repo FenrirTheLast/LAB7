a9b3239a144fae3f3c19554030a803ff2c41fbdd
FenrirTheLast <147754655+FenrirTheLast@users.noreply.github.com>
2024-06-02 11:53:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ from PIL import Image
 plt.plot([1,2,3,4])
 plt.ylabel('jakieś liczby')
 plt.show()
+
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro-')
+plt.axis((0, 6, 0, 20))
+plt.show()
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'r')
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'o')
+plt.xlim(0, 6)
+plt.ylim(0, 20)
+plt.show()
